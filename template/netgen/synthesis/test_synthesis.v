35784b5ed73f6166c080decacbac560c809e1662
Serge Hould <houlds@vaniercollege.qc.ca>
2024-09-19 15:46:52 -0400

    Signed-off-by: Serge Hould <houlds@vaniercollege.qc.ca>

---
+++ template/netgen/synthesis/test_synthesis.v
@@ -0,0 +1,136 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 1995-2013 Xilinx, Inc.  All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+//   ____  ____
+//  /   /\/   /
+// /___/  \  /    Vendor: Xilinx
+// \   \   \/     Version: P.20131013
+//  \   \         Application: netgen
+//  /   /         Filename: test_synthesis.v
+// /___/   /\     Timestamp: Thu Sep 19 15:43:38 2024
+// \   \  /  \ 
+//  \___\/\___\
+//             
+// Command	: -intstyle ise -insert_glbl true -w -dir netgen/synthesis -ofmt verilog -sim test.ngc test_synthesis.v 
+// Device	: xc3s100e-4-cp132
+// Input file	: test.ngc
+// Output file	: C:\Users\houlds\Documents\Vanier\courses_C_drive\old_program\247-503-VA_Integrated_Circuits_PLDs\projects\Basys2\Labs\template\netgen\synthesis\test_synthesis.v
+// # of Modules	: 1
+// Design Name	: test
+// Xilinx        : C:\Xilinx\14.7\ISE_DS\ISE\
+//             
+// Purpose:    
+//     This verilog netlist is a verification model and uses simulation 
+//     primitives which may not represent the true implementation of the 
+//     device, however the netlist is functionally correct and should not 
+//     be modified. This file cannot be synthesized and should only be used 
+//     with supported simulation tools.
+//             
+// Reference:  
+//     Command Line Tools User Guide, Chapter 23 and Synthesis and Simulation Design Guide, Chapter 6
+//             
+////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1 ns/1 ps
+
+module test (
+  out1, a0, a1
+);
+  output out1;
+  input a0;
+  input a1;
+  wire a0_IBUF_1;
+  wire a1_IBUF_3;
+  wire out1_OBUF_5;
+  LUT2 #(
+    .INIT ( 4'h8 ))
+  out11 (
+    .I0(a1_IBUF_3),
+    .I1(a0_IBUF_1),
+    .O(out1_OBUF_5)
+  );
+  IBUF   a0_IBUF (
+    .I(a0),
+    .O(a0_IBUF_1)
+  );
+  IBUF   a1_IBUF (
+    .I(a1),
+    .O(a1_IBUF_3)
+  );
+  OBUF   out1_OBUF (
+    .I(out1_OBUF_5),
+    .O(out1)
+  );
+endmodule
+
+
+`ifndef GLBL
+`define GLBL
+
+`timescale  1 ps / 1 ps
+
+module glbl ();
+
+    parameter ROC_WIDTH = 100000;
+    parameter TOC_WIDTH = 0;
+
+//--------   STARTUP Globals --------------
+    wire GSR;
+    wire GTS;
+    wire GWE;
+    wire PRLD;
+    tri1 p_up_tmp;
+    tri (weak1, strong0) PLL_LOCKG = p_up_tmp;
+
+    wire PROGB_GLBL;
+    wire CCLKO_GLBL;
+
+    reg GSR_int;
+    reg GTS_int;
+    reg PRLD_int;
+
+//--------   JTAG Globals --------------
+    wire JTAG_TDO_GLBL;
+    wire JTAG_TCK_GLBL;
+    wire JTAG_TDI_GLBL;
+    wire JTAG_TMS_GLBL;
+    wire JTAG_TRST_GLBL;
+
+    reg JTAG_CAPTURE_GLBL;
+    reg JTAG_RESET_GLBL;
+    reg JTAG_SHIFT_GLBL;
+    reg JTAG_UPDATE_GLBL;
+    reg JTAG_RUNTEST_GLBL;
+
+    reg JTAG_SEL1_GLBL = 0;
+    reg JTAG_SEL2_GLBL = 0 ;
+    reg JTAG_SEL3_GLBL = 0;
+    reg JTAG_SEL4_GLBL = 0;
+
+    reg JTAG_USER_TDO1_GLBL = 1'bz;
+    reg JTAG_USER_TDO2_GLBL = 1'bz;
+    reg JTAG_USER_TDO3_GLBL = 1'bz;
+    reg JTAG_USER_TDO4_GLBL = 1'bz;
+
+    assign (weak1, weak0) GSR = GSR_int;
+    assign (weak1, weak0) GTS = GTS_int;
+    assign (weak1, weak0) PRLD = PRLD_int;
+
+    initial begin
+	GSR_int = 1'b1;
+	PRLD_int = 1'b1;
+	#(ROC_WIDTH)
+	GSR_int = 1'b0;
+	PRLD_int = 1'b0;
+    end
+
+    initial begin
+	GTS_int = 1'b1;
+	#(TOC_WIDTH)
+	GTS_int = 1'b0;
+    end
+
+endmodule
+
+`endif
+
---
+++ template/test.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    
+// Design Name: 
+// Module Name:    
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 
+// Additional Comments
+//
+//////////////////////////////////////////////////////////////////////////////////
+module test(
+    input wire a1,a0,
+    output  wire out1
+    );
+	assign out1= a1&a0;
+endmodule
+
